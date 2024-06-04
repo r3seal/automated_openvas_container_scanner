556b585484fbfd6f0f3da3c40360a719bf63fe08
r3seal <167616227+r3seal@users.noreply.github.com>
2024-06-04 20:47:45 +0200

    Update main.py
    deleted sleep

--- inside_scripts/main.py
+++ inside_scripts/main.py
@@ -4,7 +4,6 @@ from scan import Scan
 import time
 
 if __name__ == "__main__":
-    time.sleep(1200)
     scan = Scan(open("scan_name.txt", "r").read().strip(), open("scan_password.txt", "r").read().strip())
     mail = Mail(open("mail.txt", "r").read().strip(), open("mail_password.txt", "r").read().strip())
     Main(mail, scan).run()
