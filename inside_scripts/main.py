5a1b896cd41b6880e38c8b12488376b6c62ebd6b
r3seal <167616227+r3seal@users.noreply.github.com>
2024-06-06 17:29:52 +0200

    Delete inside_scripts directory
    deleted unnecessary directories

--- inside_scripts/main.py
+++
@@ -1,9 +0,0 @@
-from general import Main
-from mail import Mail
-from scan import Scan
-import time
-
-if __name__ == "__main__":
-    scan = Scan(open("scan_name.txt", "r").read().strip(), open("scan_password.txt", "r").read().strip())
-    mail = Mail(open("mail.txt", "r").read().strip(), open("mail_password.txt", "r").read().strip())
-    Main(mail, scan).run()
